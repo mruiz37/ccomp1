3d8e168969de43e312e152b32b1a11c5703b2853
unknown <mruiz37@gmail.com>
2021-03-26 12:32:43 -0500

    Cambios en cpp

---
+++ operaciones.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main(){
+int valor1;
+int valor2;
+
+//suma
+std::cout<<"Ingrese 2 valores para aplicar las 4 operaciones básicas"<<std::endl;
+std::cin>>valor1;
+std::cin>>valor2;
+
+std::cout<<"la suma de los valores es "<<valor1+valor2<<std::endl;
+
+std::cout<<"la resta de los valores es "<<valor1- valor2<<std::endl;
+
+std::cout<<"la multiplicación de los valores es "<<valor1*valor2<<std::endl;
+
+std::cout<<"la división de los valores es "<<valor1/valor2<<std::endl;
+
+
+
+    return 0;
+}
+
